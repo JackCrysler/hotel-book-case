1387e5736a3d4c201e684a5d5c4a2e4bddc9c0d5
JackCrysler <jack_crysler@163.com>
2016-08-17 11:46:55 +0800

    come up with list page

--- src/js/calendar.js
+++ src/js/calendar.js
@@ -1,10 +1,9 @@
 define(['jquery','js/util','js/plugin'],function($,util,plugin){
     var Calendar = function(){
         this.initDate = new Date();
-        this.n = -1;
+        this.allMonthCount = 12;
         plugin.call(this,'');
-        this.tpl = '<div class="calendar"><div class="header"><em class="left-arrow back"></em><span>选择日期</span></div>' +
-            '<p class="cal-week clear"><span>天</span><span>一</span><span>二</span><span>三</span><span>四</span><span>五</span><span>六</span></p><div class="calendar-date" data="{YM}">{content}</div></div>';
+        this.tpl = '<div class="calendar"><div class="header"><em class="left-arrow back"></em><span>选择日期</span></div>months</div>';
     };
     Calendar.prototype = new plugin();
     Calendar.prototype.getDays = function(date){
@@ -43,13 +42,20 @@ define(['jquery','js/util','js/plugin'],function($,util,plugin){
     Calendar.prototype.getStartIndex = function(){
         return new Date(this.resetYear,this.resetMonth,1).getDay();
     };
-    Calendar.prototype.render = function(){
-        this.getYearMonth(this.n);
+    Calendar.prototype.render = function(count){
+        //根据count值去重置reset日期
+        this.getYearMonth(count);
+
         var startIndex = this.getStartIndex();
         var days = this.getDays(this.resetDate);
         var prevDays = this.getDays(new Date(this.resetYear,this.resetMonth-1,1));
+        var baseStr = '<div class="calendar-date clear" data="{YM}">{content}</div>';
+        var str = '<p class="ym">{YM}</p><p class="cal-week clear"><span>天</span><span>一</span><span>二</span><span>三</span><span>四</span><span>五</span><span>六</span></p>';
+        var _date = new Date();
+        var currentYear = _date.getFullYear(),
+            currentMonth = _date.getMonth() + 1,
+            currentDay = _date.getDate();
 
-        var str = '';
 
         for(var i=0; i<42; i++){
             if(i< startIndex){
@@ -57,18 +63,33 @@ define(['jquery','js/util','js/plugin'],function($,util,plugin){
             }else if(i>=(startIndex + days)){
                 str += '<span class="cal-item to-gray">'+ (i- (startIndex + days -1) ) +'</span>';
             }else{
-                str += '<span class="cal-item">'+ (i-startIndex+1) +'</span>';
+                var day = i-startIndex+1;
+                if(day == currentDay && currentMonth == this.resetMonth+1 && currentYear == this.resetYear){
+                    str += '<span class="cal-item" style="color:#24AEF2;">'+ day +'</span>';
+                }else{
+                    str += '<span class="cal-item">'+ day +'</span>';
+                }
+
             }
         }
+        str = str.replace('{YM}',this.resetYear+'-'+(this.resetMonth+1)).replace('{content}',str);
 
-        this.tpl = this.tpl.replace('{YM}',this.resetYear+'-'+(this.resetMonth+1)).replace('{content}',str);
+        baseStr = baseStr.replace('{YM}',this.resetYear+'-'+(this.resetMonth+1)).replace('{content}',str);
+        return baseStr;
 
+
+    };
+    Calendar.prototype.renderAll = function(){
+        var str = '';
+        for(var i=0 ;i< this.allMonthCount; i++){
+            str += this.render(i);
+        }
+        this.tpl = this.tpl.replace('months',str);
         this.pluginDom.innerHTML = this.tpl;
-
     };
     Calendar.prototype.show = function(targetEle,callback){
         this._show_();
-        this.render();
+        this.renderAll();
         this.targetEle = targetEle;
         this.bindEvent();
         this.callback = callback;
--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,17 @@
 define(['jquery','swiper','js/city','js/calendar'],function($,swiper,city,calendar){
+    //查看历史记录，是否存在搜索记录，如果存在就给页面默认展示上次的搜索记录
+    var searchHistory = localStorage.getItem('search-history');
+    if(searchHistory){
+        searchHistory = JSON.parse(searchHistory);
+        $('.local-city').text(searchHistory.city);
+        $('.date-in').text(searchHistory.inDate);
+        $('.date-out').text(searchHistory.outDate);
+    }else{
+        var cur = new Date();
+        $('.date-in').text(cur.getFullYear()+'-'+(cur.getMonth()+1)+'-'+cur.getDate());
+        $('.date-out').text(cur.getFullYear()+'-'+(cur.getMonth()+1)+'-'+(cur.getDate()+2))
+    }
+
     new swiper('.swiper-container',{
         loop:true,
         autoplay:4000
@@ -8,6 +21,9 @@ define(['jquery','swiper','js/city','js/calendar'],function($,swiper,city,calend
     $('.local-city').on('click',function(){
         city.show($(this));
     });
+
+
+
 
 
     //判断时间是否符合规范
@@ -20,7 +36,7 @@ define(['jquery','swiper','js/city','js/calendar'],function($,swiper,city,calend
     }
     $('.local-time').on('click',function(){
         calendar.show($('.date-in'),function(){
-            checkDate()
+            checkDate();
         });
     });
     $('.leave-time').on('click',function(){
@@ -28,4 +44,28 @@ define(['jquery','swiper','js/city','js/calendar'],function($,swiper,city,calend
             checkDate();
         });
     });
+
+
+    $('.search-btn').on('click',function(){
+        var city = $.trim($('.local-city').text()),
+            inDate = $.trim($('.date-in').text()),
+            outDate = $.trim($('.date-out').text());
+        if(city=='' || inDate=='' || outDate == ''){
+            alert('请填写完整信息')
+        }else{
+            var obj={
+                city:city,
+                inDate:inDate,
+                outDate:outDate
+            };
+
+            var ls = window.localStorage;
+            ls.setItem('search-history',JSON.stringify(obj));
+
+            var url = 'list.html?'+'city='+city+'&indate='+inDate+'&outdate='+outDate
+
+            window.location.href = url;
+        }
+    })
+
 });
